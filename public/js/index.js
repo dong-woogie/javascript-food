437d78d5061acfa8de8ececba27e4e729660860e
dong-woogie <com6511@gmail.com>
2021-04-15 21:24:13 +0900

    add image

--- public/js/index.js
+++ public/js/index.js
@@ -17,94 +17,94 @@ const _e = {
   soupDish: document.querySelector(".dish-list-wrap.soup-dish"),
   courseDish: document.querySelector(".dish-list-wrap.course-dish"),
   scrollBtn: document.querySelector(".scroll"),
-  searchBar: document.querySelector(".searchbar")
+  searchBar: document.querySelector(".searchbar"),
 };
 
 const contentURL = {
   bestSeller: "http://crong.codesquad.kr:8080/woowa/best",
   mainSlide:
-    "https://raw.githubusercontent.com/Imdonguk/javascript-food/master/data/mainSlide.json",
+    "https://raw.githubusercontent.com/dong-woogie/javascript-food/master/data/mainSlide.json",
   subSlide:
-    "https://raw.githubusercontent.com/Imdonguk/javascript-food/master/data/subSlide.json",
+    "https://raw.githubusercontent.com/dong-woogie/javascript-food/master/data/subSlide.json",
   mainDish:
-    "https://raw.githubusercontent.com/Imdonguk/javascript-food/master/data/main.json",
+    "https://raw.githubusercontent.com/dong-woogie/javascript-food/master/data/main.json",
   sideDish:
-    "https://raw.githubusercontent.com/Imdonguk/javascript-food/master/data/side.json",
+    "https://raw.githubusercontent.com/dong-woogie/javascript-food/master/data/side.json",
   soupDish:
-    "https://raw.githubusercontent.com/Imdonguk/javascript-food/master/data/soup.json",
+    "https://raw.githubusercontent.com/dong-woogie/javascript-food/master/data/soup.json",
   courseDish:
-    "https://raw.githubusercontent.com/Imdonguk/javascript-food/master/data/course.json",
-  autoComplete: "/ac/"
+    "https://raw.githubusercontent.com/dong-woogie/javascript-food/master/data/course.json",
+  autoComplete: "/ac/",
 };
 
 const tabView = new TabView({
   bestSellerEl: _e.bestSeller,
-  urlRequestData: contentURL.bestSeller
+  urlRequestData: contentURL.bestSeller,
 });
 
 const mainSlideContent = new SlideContent({
   slideListEl: _e.mainSlide.querySelector(".slide-list"),
-  naviEl: _e.mainSlide.querySelector(".slides-navi")
+  naviEl: _e.mainSlide.querySelector(".slides-navi"),
 });
 const mainSlideDots = new SlideDots({
-  dotListEl: _e.mainSlide.querySelector(".slide-dots")
+  dotListEl: _e.mainSlide.querySelector(".slide-dots"),
 });
 const mainSlideContoller = new SlideController({
   slideContent: mainSlideContent,
   slideDots: mainSlideDots,
   throttleTime: 1000,
-  urlRequestData: contentURL.mainSlide
+  urlRequestData: contentURL.mainSlide,
 });
 
 const subSlideContent = new SlideContent({
   slideListEl: _e.subSlide.querySelector(".slide-list"),
-  naviEl: _e.subSlide.querySelector(".slides-navi")
+  naviEl: _e.subSlide.querySelector(".slides-navi"),
 });
 const subSlideDots = new SlideDots({
-  dotListEl: _e.subSlide.querySelector(".slide-dots")
+  dotListEl: _e.subSlide.querySelector(".slide-dots"),
 });
 const subSlideController = new SlideController({
   slideContent: subSlideContent,
   slideDots: subSlideDots,
   throttleTime: 500,
-  urlRequestData: contentURL.subSlide
+  urlRequestData: contentURL.subSlide,
 });
 
 const mainDishSlide = new MenuSlide({
   urlRequestData: contentURL.mainDish,
   slideListEl: _e.mainDish.querySelector(".slide-list"),
-  naviEl: _e.mainDish.querySelector(".slides-navi")
+  naviEl: _e.mainDish.querySelector(".slides-navi"),
 });
 
 const sideDishSlide = new MenuSlide({
   urlRequestData: contentURL.sideDish,
   slideListEl: _e.sideDish.querySelector(".slide-list"),
-  naviEl: _e.sideDish.querySelector(".slides-navi")
+  naviEl: _e.sideDish.querySelector(".slides-navi"),
 });
 
 const soupDishSlide = new MenuSlide({
   urlRequestData: contentURL.soupDish,
   slideListEl: _e.soupDish.querySelector(".slide-list"),
-  naviEl: _e.soupDish.querySelector(".slides-navi")
+  naviEl: _e.soupDish.querySelector(".slides-navi"),
 });
 
 const courseDishSlide = new MenuSlide({
   urlRequestData: contentURL.courseDish,
   slideListEl: _e.courseDish.querySelector(".slide-list"),
-  naviEl: _e.courseDish.querySelector(".slides-navi")
+  naviEl: _e.courseDish.querySelector(".slides-navi"),
 });
 
 const scrollBtnView = new ScrollBtnView({
-  scrollEl: _e.scrollBtn
+  scrollEl: _e.scrollBtn,
 });
 
 const autoComplete = new AutoComplete({
   searchBarEl: _e.searchBar,
-  urlRequestData: contentURL.autoComplete
+  urlRequestData: contentURL.autoComplete,
 });
 
 function init() {
-  _e.body.addEventListener("click", e => {
+  _e.body.addEventListener("click", (e) => {
     if (e.target.tagName !== "A") return;
     e.preventDefault();
   });
